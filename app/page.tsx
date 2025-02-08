bcd36f4067d079769ec810f25a56df43811cbdff
Karanjot Gaidu <90838376+karanjot-gaidu@users.noreply.github.com>
2025-02-08 15:34:11 -0500

    3d Draft (#2)

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,30 @@
 "use client"
-import {useUser } from "@clerk/nextjs"
+import { useUser } from "@clerk/nextjs"
 import "./home.css"
 import LoginPage from "./components/login-page"
+import SolarSystemModel from "./components/3d-model"
+import NavBar from "./components/nav-bar"
 
 export default function Home() {
   const { isSignedIn, user } = useUser();
   console.log(user);
   return (
-    <>
-     {/* Commented out to keep a reference to login button to be used later */}
-      {/* <div className="relative flex gap-3">
-        <SignedIn>
-          <Link
-            href="/dashboard"
-            className="px-4 py-2 rounded-full bg-[#131316] text-white text-sm font-semibold"
-          >
-            Dashboard
-          </Link>
-        </SignedIn>
-        <SignedOut>
-          <SignInButton>
-            <button className="px-4 py-2 rounded-full bg-[#131316] text-white text-sm font-semibold">
-              Sign in
-            </button>
-          </SignInButton>
-        </SignedOut>
-      </div> */}
-      
-      
-        
-
-        <div className="flex justify-center items-center min-h-screen">
+      <div className="relative min-h-screen">
+        <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: "url('/space.jpg')" }}></div>
+        <NavBar />
+          
+        <div className="flex justify-center items-center min-h-screen relative z-10">
           <div className="text-center">
             {isSignedIn ? (
-              <h1 className="text-xl font-semibold">Welcome, {user?.fullName}!</h1>
+                <div>
+                  <h1 className="text-xl font-semibold inline">Welcome, {user?.fullName}!</h1>
+                  <div><SolarSystemModel /></div>
+                </div>
             ) : (
-              <>
-                <LoginPage/>
-              </>
+              <LoginPage />
             )}
           </div>
         </div>
-
-    </>
-  )
+      </div>
+  );
 }
